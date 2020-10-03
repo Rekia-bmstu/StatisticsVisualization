2ca2b1103dadf914a5de5aaec7c0776c81fca65c
Alexander Baziev <bazievalex2@gmail.com>
2020-10-03 09:02:01 +0300

    Added MathVector Collection

---
+++ VectorCollection/MathVector.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VectorCollection
+{
+    public class MathVector : IEnumerable<double>
+    {
+        private List<double> _data;
+
+        public MathVector(params double[] data)
+        {
+            _data = new List<double>();
+            foreach (var item in data)
+            {
+                _data.Add(item);
+            }
+        }
+
+        public MathVector(List<double> data)
+        {
+            _data = data;
+        }
+
+        public MathVector(int size)
+        {
+            _data = new List<double>(size);
+        }
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            return new VectorEnumerator(_data);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public MathVector AddNumber(double n)
+        {
+            return new MathVector(_data.Select(elem => elem + n).ToList());
+        }
+
+        public static MathVector operator +(MathVector vector, double n)
+        {
+            return vector.AddNumber(n);
+        }
+
+        public MathVector SubstractNumber(double n)
+        {
+            return new MathVector(_data.Select(elem => elem - n).ToList());
+        }
+
+        public static MathVector operator -(MathVector vector, double n)
+        {
+            return vector.SubstractNumber(n);
+        }
+
+        public MathVector DivideByNumber(double n)
+        {
+            if (n != 0)
+            {
+                throw new ArgumentException("The division number is zero");
+            }
+
+            return new MathVector(_data.Select(elem => elem / n).ToList());
+        }
+
+        public static MathVector operator /(MathVector vector, double n)
+        {
+            return vector.DivideByNumber(n);
+        }
+
+        public MathVector MulitplyByNumber(double n)
+        {
+            return new MathVector(_data.Select(elem => elem * n).ToList());
+        }
+
+        public static MathVector operator*(MathVector vector, double n)
+        {
+            return vector.MulitplyByNumber(n);
+        }
+    }
+}
---
+++ VectorCollection/VectorEnumerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorCollection
+{
+    internal class VectorEnumerator : IEnumerator<double>
+    {
+        private List<double> _data;
+        private double _current;
+        private int _pos = -1;
+        private bool disposing;
+
+        public double Current
+        {
+            get 
+            {
+                if (_pos > _data.Count || _pos < 0)
+                    throw new Exception("?????????");
+                
+                return _current;
+            }
+
+            private set 
+            {
+                _current = value;
+            }
+        }
+
+        public VectorEnumerator(List<double> data)
+        {
+            _data = data;
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            disposing = true;
+        }
+
+        public bool MoveNext()
+        {
+            if (_pos + 1 >= _data.Count)
+                return false;
+            
+            _pos++;
+            _current = _data[_pos];
+            return true;
+        }
+
+        public void Reset()
+        {
+            _pos = -1;
+        }
+    }
+}
