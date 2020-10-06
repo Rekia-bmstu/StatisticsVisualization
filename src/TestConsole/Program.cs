189a79603d643aa8615ecd816a7be9f217317eb0
Alexander Baziev <bazievalex2@gmail.com>
2020-10-06 22:05:38 +0300

    Added src

---
+++ src/TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using VectorCollection;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathVector mathVector = new MathVector(1, 2, 3, 4, 5);
+            MathVector mathVector2 = new MathVector(1, 2, 3, 4, 5);
+
+        }
+    }
+}
