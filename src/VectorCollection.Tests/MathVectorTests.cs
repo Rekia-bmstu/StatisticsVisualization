e4c3c09dbef5db8201c8e3a2a32241e9dc347037
Alexander Baziev <bazievalex2@gmail.com>
2020-11-05 19:33:51 +0300

    Added tests

---
+++ src/VectorCollection.Tests/MathVectorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VectorCollection.Tests
+{
+    public class MathVectorTests
+    {
+        [Fact]
+        public void SumPositiveNumberTest()
+        {
+            var vector = new MathVector(-1, 0, 1);
+            var sum_vector = vector + 1;
+            Assert.Equal(new MathVector(0, 1, 2), sum_vector);
+        }
+    }
+}
