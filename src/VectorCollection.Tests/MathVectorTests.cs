3e6fd09c06f59faec06535c340d7e3053e26490a
Alexander Baziev <bazievalex2@gmail.com>
2020-11-06 14:40:43 +0300

    Added tests

--- src/VectorCollection.Tests/MathVectorTests.cs
+++ src/VectorCollection.Tests/MathVectorTests.cs
@@ -10,10 +10,10 @@ namespace VectorCollection.Tests
     public class MathVectorTests
     {
         [Fact]
-        public void SumPositiveNumberTest()
+        public void SumPositiveNumber()
         {
             var vector = new MathVector(-1, 0, 1);
-            Assert.Equal<MathVector>(new MathVector(0, 1, 2), vector + 1);
+            Assert.Equal<MathVector>(new MathVector(0, 1, 2), vector.AddNumber(1));
         }
 
         [Fact]
@@ -28,6 +28,57 @@ namespace VectorCollection.Tests
         {
             var vector = new MathVector(-1, 0, 1);
             Assert.Equal<MathVector>(new MathVector(-1, 0, 1), vector + 0);
+        }
+
+        [Fact]
+        public void SumPositiveVectors()
+        {
+            var vector1 = new MathVector(1, 2, 3);
+            var vector2 = new MathVector(0, 1, 1);
+
+            Assert.Equal(expected: new MathVector(1, 3, 4), actual: vector1 + vector2);
+        }
+
+        [Fact]
+        public void SumNegativeVectors()
+        {
+            var vector1 = new MathVector(1, 2, 3);
+            var vector2 = new MathVector(-1, -1, -1);
+
+            Assert.Equal(expected: new MathVector(0, 1, 2), actual: vector1 + vector2);
+        }
+
+        [Fact]
+        public void SumZeroVectors()
+        {
+            var vector1 = new MathVector(1, 2, 3);
+            var vector2 = new MathVector(0, 0, 0);
+
+            Assert.Equal(expected: new MathVector(1, 2, 3), actual: vector1 + vector2);
+        }
+
+        [Fact]
+        public void MultiplyByPositiveNumber()
+        {
+            var vector = new MathVector(1, 2, 3);
+
+            Assert.Equal(expected: new MathVector(2, 4, 6), actual: vector * 2);
+        }
+
+        [Fact]
+        public void MultiplyByNegativeNumber()
+        {
+            var vector = new MathVector(1, 2, 3);
+
+            Assert.Equal(expected: new MathVector(-1, -2, -3), actual: vector * -1);
+        }
+
+        [Fact]
+        public void MultiplyByZeroNumber()
+        {
+            var vector = new MathVector(1, 2, 3);
+
+            Assert.Equal(expected: new MathVector(0, 0, 0), actual: vector * 0);
         }
     }
 }
